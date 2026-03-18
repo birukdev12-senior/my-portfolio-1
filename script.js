7ddd2a48f5d6cccf1753e7b9b01053f679238b2e
btsegaye012-crypto <btsegaye012@gmail.com>
2026-03-18 23:19:35 +0300

    Update and rename Script.js to script.js
    This script handles the core logic of the app. It uses the fetch() method to call the OpenWeatherMap API, processes the JSON data, and dynamically updates the HTML elements (City name, Temperature, and Description) based on the user's search input.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "1dc9902f3c99519ed3cc0de0b8242ad2";
+const apiKey ="1dc9902f3c99519ed3cc0de0b8242ad2";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
@@ -6,10 +6,12 @@ const searchBtn = document.querySelector(".search button");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    if(response.status == 404){
-        alert("ከተማው አልተገኘም! እባክህ ስሙን አስተካክል");
+
+    if(response.status == 404) {
+        alert("የከተማው ስም ተሳስቷል! እባክህ ድጋሚ ሞክር።");
     } else {
         var data = await response.json();
+
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".description").innerHTML = data.weather[0].description;
